bd0fdbb890ab0d70939f729a77655cc3e4b9d15d
pnbakken <74681160+pnbakken@users.noreply.github.com>
2021-08-28 01:33:27 +0200

    sorting and tidying code

--- js/question-2.js
+++ js/question-2.js
@@ -1,9 +1,9 @@
 const apiKey = "c989a2e2ef644b9db6ec4a8c9e8952fc";
 const rawgURL = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
 const corsURL = "https://noroffcors.herokuapp.com/" + rawgURL; // added if neccessary but so far so good.
+
 const resultContainer = document.querySelector("#result-container")
 const sectionHeading = document.querySelector(".section-header");
-console.log(document);
 
 async function fetchFromApi() {
     sectionHeading.innerHTML = `<div class="loading-spinner">Fetching from API</div>`;
