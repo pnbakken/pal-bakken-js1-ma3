776c00a11a90286d7370b1a62bc3d3359ef820fe
pnbakken <74681160+pnbakken@users.noreply.github.com>
2021-08-27 23:40:02 +0200

    added API call

--- js/question-2.js
+++ js/question-2.js
@@ -1,2 +1,8 @@
 const apiKey = "c989a2e2ef644b9db6ec4a8c9e8952fc";
 const rawgURL = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
+
+async function fetchFromApi() {
+    const response = await fetch(rawgURL);
+    const result = await response.json();
+    console.log(result);
+}
