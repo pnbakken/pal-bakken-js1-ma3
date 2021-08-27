c9c4a0585b0f34c6bd6e21ec4f8f622955c24e02
pnbakken <74681160+pnbakken@users.noreply.github.com>
2021-08-28 01:19:17 +0200

    got access to document elements. added script tags to page head but forgot to set scripts to defer until after document finishes loading. fixed now.

--- js/question-2.js
+++ js/question-2.js
@@ -2,7 +2,7 @@ const apiKey = "c989a2e2ef644b9db6ec4a8c9e8952fc";
 const rawgURL = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
 const corsURL = "https://noroffcors.herokuapp.com/" + rawgURL; // added if neccessary but so far so good.
 const resultContainer = document.querySelector("#result-container")
-console.log(resultContainer);
+console.log(document);
 
 async function fetchFromApi() {
     
