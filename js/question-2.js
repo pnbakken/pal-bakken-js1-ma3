7b5e7252a3001ef1701a82fdf3efb4f332387b4f
pnbakken <74681160+pnbakken@users.noreply.github.com>
2021-08-28 16:36:42 +0200

    small changes

--- js/question-2.js
+++ js/question-2.js
@@ -1,6 +1,6 @@
 const apiKey = "c989a2e2ef644b9db6ec4a8c9e8952fc";
 const rawgURL = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
-const corsURL = "https://noroffcors.herokuapp.com/" + rawgURL; // added if neccessary but so far so good.
+const corsURL = "https://noroffcors.herokuapp.com/" + rawgURL; // added if neccessary but not needed so far.
 
 const resultContainer = document.querySelector("#result-container")
 const sectionHeading = document.querySelector(".section-header");
@@ -16,8 +16,11 @@ async function fetchFromApi() {
             for (let [index, item] of result.results.entries()) { // could do this with a regular for loop, but I've gotten into the habit of using for-of.
                 if(index === 8) {
                     break;
+                    //this break seems to work fine on its own, without the 'else' statement. I suppose that's how 'break' functions, but I added an 'else' to be safe.
+                } else {
+                    outputItem(index, buildItem(item), resultContainer);
                 }
-                outputItem(buildItem(item), resultContainer);
+                
             }
         } 
     } catch (err) {
@@ -28,26 +31,39 @@ async function fetchFromApi() {
 
 function buildItem(rawItem) {
 
-    var cleanItem = {
+    /* 
+       It seems handy to check what we received from the API call
+       and use that info to create new objects to use for the HTML, 
+       rather than plug in the objects directly from the API and 
+       potentially receive errors related to object properties. 
+
+       I'm not sure the default values I've provided neccessarily 
+       make sense in the larger scheme of whatever website this
+       would be made for.
+    */
+
+    var newItem = {
         name: "No name",
         rating: "No rating",
         tags: [],
     }
 
     if (rawItem.name) {
-        cleanItem.name = rawItem.name;
+        newItem.name = rawItem.name;
     }
     if (rawItem.rating) {
-        cleanItem.rating = rawItem.rating;
+        newItem.rating = rawItem.rating;
     }
     if(rawItem.tags) {
-        cleanItem.tags = rawItem.tags;
+        newItem.tags = rawItem.tags;
     }
 
-    return cleanItem;
+    return newItem;
 }
 
-function outputItem(item, container) {
+function outputItem(index, item, container) {
+    // Not really sure i need the game number id. I might just be adding whatever stuff I can think to do.
+    // Should change the html for this.
     container.innerHTML += `<div class="item">
                                 <ul>
                                     <li>Name: ${item.name}</li>
